1c5054f9fdefbe1bed0bb260059b0a3b6f758be7
serhiiqw <sergeikhodos6@gmail.com>
2025-12-07 13:47:33 -0500

    Loop for questions

--- js/script.js
+++ js/script.js
@@ -1,9 +1,4 @@
-
-const numberOfFims = +prompt('How many films have you watched?', '');
-const secondQuestion = prompt('The last movie you have watched?', '');
-    thirdQuestion = +prompt('Value this film from 0 to 10', '');
-    lastMovieAnswer = secondQuestion;
-    lastMovieValueAnswer = thirdQuestion;
+let numberOfFims = +prompt('How many films have you watched?', '');
 
 let personalMovieDB = {
     count: numberOfFims,
@@ -13,7 +8,31 @@ let personalMovieDB = {
     privat: false
 }
 
-personalMovieDB.movies[lastMovieAnswer] = lastMovieValueAnswer;
+for (let i = 0; i < 1; i++) {
+    aQuestion = prompt('The last movie you have watched?', '');
+    bQuestion = +prompt('Value this film from 0 to 10', '');
+
+    if (aQuestion != null && bQuestion != null && aQuestion != '' && bQuestion != '' && aQuestion.length < 50) {
+        personalMovieDB.movies[aQuestion] = bQuestion;
+        break;
+    } else {
+        i--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    alert('You have watched not too much movies');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('You are classic watcher');
+} else if (personalMovieDB.count >= 30) {
+    alert('You are a huge fan');
+} else {
+    alert('Error');
+}
+
+console.log(personalMovieDB);
+
+
 
 
 
